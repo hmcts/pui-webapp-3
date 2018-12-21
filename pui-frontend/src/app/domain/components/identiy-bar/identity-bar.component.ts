ccda9f4a26320f8cdd6410d4d70e4b70e3cf3546
fazanki <fazanki@gmail.com>
2018-12-21 16:04:08 +0000

    adding dinamic mechanisam for button

--- pui-frontend/src/app/domain/components/identiy-bar/identity-bar.component.ts
+++ pui-frontend/src/app/domain/components/identiy-bar/identity-bar.component.ts
@@ -6,10 +6,30 @@ import 'rxjs/add/operator/filter';
     templateUrl: './identity-bar.component.html',
     styleUrls: ['./identity-bar.component.scss']
 })
-export class IdentityBarComponent {
+export class IdentityBarComponent implements OnInit{
 
     @Input() case: any;
+    menuItems: {[id: string]: string}[];
 
     constructor() {}
+
+    ngOnInit(): void {
+        this.menuItems = [
+            {
+                action: '../decision/create',
+                text: 'Make decision',
+                type: 'button'
+            },
+            {   action: '../hearing/list',
+                text: 'List for hearing',
+                classes: 'hmcts-button--secondary',
+                type: 'link'
+            }
+        ]
+    }
+
+    onButtonSubmit(payload): void {
+        console.log(payload)
+    }
 
 }
--- pui-frontend/src/app/shared/components/menu/menu.component.ts
+++ pui-frontend/src/app/shared/components/menu/menu.component.ts
@@ -1,14 +1,23 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
     selector: 'app-menu',
     templateUrl: './menu.component.html',
     styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
 
-    @Input() items: {[id: string]: string};
+    @Input() items: {[id: string]: string}[];
+    @Output() onButtonSubmit = new EventEmitter<string>();
 
     constructor() { }
+
+    ngOnInit(): void {
+
+    }
+
+    onActionSubmit(payload): void {
+        this.onButtonSubmit.emit(payload.action);
+    }
 
 }
