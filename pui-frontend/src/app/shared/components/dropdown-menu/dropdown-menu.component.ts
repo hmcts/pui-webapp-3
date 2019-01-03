f9c3b6521807c1dc1f65856b85282b7c40cffced
fazanki <fazanki@gmail.com>
2019-01-03 13:18:53 +0000

    attempt to set the focus

--- pui-frontend/src/app/shared/components/dropdown-menu/dropdown-menu.component.ts
+++ pui-frontend/src/app/shared/components/dropdown-menu/dropdown-menu.component.ts
@@ -7,23 +7,48 @@ import {Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output
     styleUrls: ['./dropdown-menu.component.scss']
 })
 export class DropdownMenuComponent implements OnInit {
-    @Input() items
-    @Input() config
+    @Input() items;
+    @Input() config;
+
     @ViewChild('optionsDropdown') optionsDropdown: ElementRef;
+
     styles = {display: 'none'};
+    isExpanded = false;
+    rank: number;
 
     constructor() { }
 
     @HostListener('document: click', ['$event.target'])
-    onClick(target) {
+    onClick(target): void {
         if (target.className === 'govuk-button hmcts-menu__toggle-button' ) {
             if (this.optionsDropdown.nativeElement.offsetParent === null) {
-                this.styles.display = 'block'
+                this.styles.display = 'block';
+                this.isExpanded = true;
+                // TODO revisit the focussing.
+                this.rank = 0;
+                setTimeout(() => {
+                    this.optionsDropdown.nativeElement.children[this.rank].focus();
+                }, 200)
             } else {
-                this.styles.display = 'none'
+                this.styles.display = 'none';
+                this.isExpanded = false;
             }
         } else {
             this.styles.display = 'none'
+        }
+    }
+
+    @HostListener('keydown', ['$event'])
+    browseOptionsList(event) {
+        if (this.optionsDropdown.nativeElement.offsetParent !== null) {
+            const nodeLength = this.optionsDropdown.nativeElement.children.length;
+            if (this.rank < nodeLength - 1) {
+                this.rank++;
+            } else {
+                this.rank = 0;
+            }
+            this.optionsDropdown.nativeElement.children[this.rank].focus()
+
         }
     }
 
